9ddb43d9951386b28851a3d49e3c43b50e9a34d0
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2025-07-10 09:27:41 +0200

    Add RSS feed screen

---
+++ apps/frontend/app/app/(monitor)/rss-feed/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontFamily: 'Poppins_700Bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  body: {
+    fontSize: 16,
+    fontFamily: 'Poppins_400Regular',
+    textAlign: 'center',
+  },
+});
