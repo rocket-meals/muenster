e0d6852fc47a3439a8f8d672dcc6b0c93f3b96ea
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2025-07-16 17:42:08 +0200

    feat(frontend): add stack header for chats

---
+++ apps/frontend/app/app/(app)/chats/_layout.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useTheme } from '@/hooks/useTheme';
+import { Stack } from 'expo-router';
+import CustomStackHeader from '@/components/CustomStackHeader/CustomStackHeader';
+import { useLanguage } from '@/hooks/useLanguage';
+import { TranslationKeys } from '@/locales/keys';
+
+export default function ChatsLayout() {
+  const { theme } = useTheme();
+  const { translate } = useLanguage();
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: theme.header.background },
+        headerTintColor: theme.header.text,
+      }}
+    >
+      <Stack.Screen
+        name='index'
+        options={{
+          header: () => (
+            <CustomStackHeader label={translate(TranslationKeys.chats)} />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name='details/index'
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+}
