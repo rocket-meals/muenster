eca83fd418a5977a26a77e6ccd690e351957153f
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2025-07-18 02:24:35 +0200

    Add extended placeholder text to DistanceModal

--- apps/frontend/app/components/DistanceModal/DistanceModal.tsx
+++ apps/frontend/app/components/DistanceModal/DistanceModal.tsx
@@ -59,6 +59,11 @@ const DistanceModal: React.FC<DistanceModalProps> = ({
             'Wir teilen deinen aktuellen Standort nicht mit uns. Er wird ausschließlich auf deinem Handy verwendet, um die Entfernung zu berechnen. Aus Datenschutzgründen verlassen diese Daten niemals dein Gerät und werden nicht gespeichert. So kannst du sicher sein, dass deine Privatsphäre geschützt ist, während du den vollen Funktionsumfang testen kannst.'
           }
         </Text>
+        <Text style={{ color: theme.screen.text }}>
+          {
+            'Lorem insulin dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+          }
+        </Text>
       </ScrollView>
     </BaseBottomModal>
   );
