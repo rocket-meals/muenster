c63f485f5b7193f35649af63f23d9867752c6995
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2025-07-23 01:26:14 +0200

    index.tsx aktualisieren

--- apps/frontend/app/app/(app)/vertical-scroll-top-food/index.tsx
+++ apps/frontend/app/app/(app)/vertical-scroll-top-food/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import useSetPageTitle from '@/hooks/useSetPageTitle';
-import { TranslationKeys } from '@/locales/keys';
-import VerticalScrollTopFood from '@/components/VerticalScrollTopFood';
+import React, { useEffect, useState } from 'react';
+import { Image, View, ActivityIndicator } from 'react-native';
+import QRCode from 'qrcode';
 
 const VerticalScrollTopFoodScreen = () => {
-  useSetPageTitle(TranslationKeys.vertical_scroll_top_food);
-  return <VerticalScrollTopFood />;
+  const [qrUri, setQrUri] = useState<string | null>(null);
+
+  useEffect(() => {
+    const googleUrl = 'https://www.google.com/search?q=Expo+QR+Code+Generator';
+
+    QRCode.toDataURL(googleUrl, { errorCorrectionLevel: 'H' })
+      .then(setQrUri)
+      .catch(console.error);
+  }, []);
+
+  if (!qrUri) return <ActivityIndicator />;
+
+  return (
+    <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+      <Image
+        source={{ uri: qrUri }}
+        style={{ width: 200, height: 200 }}
+        resizeMode="contain"
+      />
+    </View>
+  );
+
 };
 
 export default VerticalScrollTopFoodScreen;
