25a4fc3808640f487853e36a7610066a8862f38f
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2025-07-23 13:16:02 +0200

    fix: prevent QR code logo cropping

--- apps/frontend/app/components/QrCode/index.tsx
+++ apps/frontend/app/components/QrCode/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Image, View } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
 import { QrCodeProps } from './types';
 
@@ -6,11 +7,27 @@ const QrCode: React.FC<QrCodeProps> = ({ value, size = 200, logoSource, logoUrl 
   const logo = logoSource ? logoSource : logoUrl ? { uri: logoUrl } : undefined;
 
   return (
-    <QRCode
-      value={value}
-      size={size}
-      {...(logo ? { logo, logoSize: size / 5, logoBackgroundColor: 'transparent' } : {})}
-    />
+    <View style={{ width: size, height: size }}>
+      <QRCode value={value} size={size} />
+      {logo && (
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Image
+            source={logo}
+            style={{ width: size / 5, height: size / 5, resizeMode: 'contain' }}
+          />
+        </View>
+      )}
+    </View>
   );
 };
 
