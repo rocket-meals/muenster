eadd3d203e91f33f47ef956108f4119b815e8101
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2025-07-23 02:45:52 +0200

    fix(web): show QR codes correctly and add generator demos

---
+++ apps/frontend/app/app/(app)/experimentell/qrcode-generator/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainer: {},
+  content: {
+    width: '100%',
+    height: '100%',
+    padding: 20,
+    alignItems: 'center',
+  },
+  heading: {
+    fontSize: 24,
+    fontFamily: 'Poppins_700Bold',
+    marginVertical: 10,
+  },
+  qr: {
+    width: 200,
+    height: 200,
+    marginTop: 20,
+  },
+});
