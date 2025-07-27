42ed6707c533fbefb691c18c1e30f8eb5ddb8005
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2025-07-27 11:59:44 +0200

    fix(leaflet): use iconUrl for web markers

--- apps/frontend/app/components/MyMap/markerUtils.ts
+++ apps/frontend/app/components/MyMap/markerUtils.ts
@@ -11,10 +11,13 @@ export class MyMapMarkerIcons {
   static DEBUG_ICON = `<div style='width: ${MARKER_DEFAULT_SIZE}px; height: ${MARKER_DEFAULT_SIZE}px; background-color: #FF000066; position: relative;'><div style='width: ${MARKER_DEFAULT_SIZE}px; height: ${MARKER_DEFAULT_SIZE}px; background-color: #00FF0066; border-radius: 50%; position: absolute; top: 0%; left: 0%;'></div></div>`;
 
   static getIconForWebByUri(iconUri: string): string {
-    return `<img src='${iconUri}' style='width: ${MARKER_DEFAULT_SIZE}px; height: ${MARKER_DEFAULT_SIZE}px; object-fit: contain;'>`;
+    // Only return the URI so the webview can create the <img> tag itself
+    return iconUri;
   }
 
   static getIconForWebByBase64(base64: string): string {
-    return `<img src='data:image/png;base64,${base64}' style='width: ${MARKER_DEFAULT_SIZE}px; height: ${MARKER_DEFAULT_SIZE}px; object-fit: contain;'>`;
+    // Only return the base64 data URI. The leaflet web page will convert this
+    // string into an <img> tag.
+    return `data:image/png;base64,${base64}`;
   }
 }
