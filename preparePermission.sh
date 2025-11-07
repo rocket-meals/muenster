96017a50cfe5cdc86f2d319b3faa3f27c4cd7764
Nils <nilsbaumgartner1994@gmail.com>
2025-11-07 09:35:49 +0100

    Improve Permission Setup Script

--- preparePermission.sh
+++ preparePermission.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 echo "Setting read/write permission for uploads"
-mkdir -p ./database_file_uploads/
-chmod -R 777 ./database_file_uploads/
+mkdir -p ./data/database_file_uploads/
+chmod -R 777 ./data/database_file_uploads/
 
 echo "Setting read/write permission for database"
 # Ensure the database directory exists
-mkdir -p ./database/
+mkdir -p ./data/database/
 # Set read/write/execute permissions for all users
-chmod -R 777 ./database/
+chmod -R 777 ./data/database/
 
 echo "Setting read/write permission for database backups"
 # Ensure the database backups directory exists
-mkdir -p ./database_backups/
+mkdir -p ./data/database_backups/
 # Set read/write/execute permissions for all users
-chmod -R 777 ./database_backups/
+chmod -R 777 ./data/database_backups/
+
+echo "Setting read/write permission for .env file"
+# Set read/write permissions for all users on the .env file
+chmod 666 .env
 
 echo "Finished"
